a3dc2c19640e16332a40b6fb324259043d008c51
sarathduggirala <52659658+sarathduggirala@users.noreply.github.com>
2019-08-02 16:57:59 +0530

    Update HideUnnecessaryChannels.js

--- Week3/Week3/js/HideUnnecessaryChannels.js
+++ Week3/Week3/js/HideUnnecessaryChannels.js
@@ -2,9 +2,9 @@ export default function hideUnnecessaryChannel(numberOfDivisionsToBeHidden) {
   let divisionsToBeHidden = document.getElementById("authors-division")
     .childNodes;
   for (
-    let i = divisionsToBeHidden.length - numberOfDivisionsToBeHidden;
-    i < divisionsToBeHidden.length;
-    i++
+    let count = divisionsToBeHidden.length - numberOfDivisionsToBeHidden;
+    count < divisionsToBeHidden.length;
+    count++
   ) {
     divisionsToBeHidden[i].style.display = "none";
   }
